4953ddb3946940287b70fe78dbf2c16c71187a01
Akshit Bhutani <125668554+akshit-bhutani-19@users.noreply.github.com>
2024-09-20 13:11:27 +0530

    Update index.tsx

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -8,7 +8,7 @@ export default function Footer() {
       <p>
         Made by{' '}
         <a href='https://github.com/ayushiee/nextflix' target='_blank' rel='noreferrer'>
-          <b>Aarushi</b>
+          <b>Akshit</b>
         </a>
       </p>
     </div>
